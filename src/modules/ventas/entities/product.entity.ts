dfc07e9185927b489538dba26c9600ffc2c8cf59
SANTIAGO_DAVID_GUALLICHICO_QUISHPE <sdq.guallichico@yavirac.edu.ec>
2023-01-27 12:21:13 -0500

    primer commit-corregir errores

---
+++ src/modules/ventas/entities/product.entity.ts
@@ -0,0 +1,144 @@
+import { CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn, } from "typeorm";
+
+@Entity('products',{schema:'ventas'} )
+
+
+export class ProductEntity{
+    @PrimaryGeneratedColumn('venta')
+    id:string;
+
+    @CreateDateColumn({
+        name: 'created_date',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+    createdAt:Date;
+
+    @UpdateDateColumn({
+        name: 'updated_date',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+    updateAt:Date;
+
+    @DeleteDateColumn({
+        name: 'updated_date',
+        type:'timestamptz',
+        nullable:true ,
+    })
+    deleteAt:Date;
+
+    //relaciones
+    @ManyToOne(() => CategoryEntity, (category) => category.product)
+    category: CategoryEntity;
+
+
+    @Column('varchar',{
+        name:'title',
+        unique:true,
+        comment: 'titulo del producto',
+    })
+
+    title:string;
+
+}
+
+SANTIAGO DAVID GUALLICHICO QUISHPE, lun 15:31
+-------------------------------------------------------------------
+import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn, } from "typeorm";
+
+@Entity('products',{schema:'ventas'} )
+
+
+export class ProductEntity{
+    @PrimaryGeneratedColumn('venta')
+    id:string;
+
+    @CreateDateColumn({
+        name: 'created_date',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+    createdAt:Date;
+
+    @UpdateDateColumn({
+        name: 'updated_date',
+        type:'timestamptz',
+        default:() => 'CURRENT_TIMESTAMP',
+    })
+    updateAt:Date;
+
+    @DeleteDateColumn({
+        name: 'updated_date',
+        type:'timestamptz',
+        nullable:true ,
+    })
+    deleteAt:Date;
+
+    //relaciones
+    @ManyToOne(() => CategoryEntity, (category) => category.product)
+    category: CategoryEntity;
+
+
+    @Column('varchar',{
+        name:'title',
+        unique:true,
+        comment: 'titulo del producto',
+    })
+
+    title:string;
+
+    @Column('number',{
+        name:'price',
+        comment: 'precio del producto con dos decimales',
+    })
+
+    price:number;
+
+    @Column('varchar',{
+        name:'description',
+        nullable:true,
+        comment: 'descripcion del producto ',
+    })
+
+    description:string;
+
+    // @Column('boolean',{
+    //     name:'estate',
+    //     comment: 'estado del producto ',
+    // })
+
+    // estado:boolean;
+
+
+    // @Column('enum',{
+    //     name:'language',
+    //     enum:languageTypeEnum,  
+    //   })
+    //   language:LanguageTypeEnum;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async setTitle(){
+        if(!this.title){
+            return;
+        }
+        this.title = this.title.toUpperCase();
+    }
+    // setEmail(){
+    //     if(!this.email){
+    //         return;
+    //     }
+    //     this.email = this.email.toLowerCase().trim();
+    // }
+
+
+    // async hashPassword(){
+    //     if(!this.password)
+    //     return;
+    // }
+    // this.password = await Bcrypto.hash(this.password, 12);
+
+    // completar los dos campos que faltan y hacer de categoria
+
+}
